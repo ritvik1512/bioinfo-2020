b2f48fe1829b225baa32e78691ef8c5943aa5827
ritvik1512 <ritvik@outlook.com>
2020-05-29 18:19:53 +0900

    手動アライン

--- align_scibio.py
+++ align_scibio.py
@@ -1,24 +1,12 @@
+from Bio import pairwise2
+from Bio.pairwise2 import format_alignment
 import sys
 import re
 from random import seed
 from random import randint
 
-# スコア情報設定
-class score:
-    def __init__(self, match, mismatch, gap, gap_ex):
-        self.match = match
-        self.mismatch = mismatch
-        self.gap = gap # gap開始ペナルティー
-        self.gap_ex = gap_ex # gap伸長ペナルティー
+regex = re.compile(r'>')
 
-    def matching(self, X, Y):
-        # match確認
-        if X == Y:
-            return self.match
-        else:
-            return self.mismatch
-
-# FASTA入力の解析
 def parse_fa(input):
     head = None
     sequence =  []
@@ -36,16 +24,21 @@ def align(X,Y):
     X_seq = str("First seq: "+ X +"\n")
     Y_seq = str("Second seq: "+ Y +"\n\n")
 
-    with open("man_output.txt", "w") as out:
+    alignment = pairwise2.align.globalms(X, Y, 1, -1, -2, -1)
+
+    with open("output.txt", "w") as out:
         out.write(X_seq)
         out.write(Y_seq)
+        for al in alignment:
+            #print(format_alignment(*al))
+            out.write(format_alignment(*al))
+
 
 if __name__ == "__main__":
     # 初期化
     header = []
     seq = []
     seed(1)
-    regex = re.compile(r'>')
 
     # FASTAの標準入力
     inFile = sys.argv[1]  
